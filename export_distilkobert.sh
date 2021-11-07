8336eb31ba93bc3bd9a5797021cd5a7ebe62a436
Jeong Ukjae <JeongUkJae@gmail.com>
2021-11-07 16:27:19 +0900

    update model name

--- export_distilkobert.sh
+++ export_distilkobert.sh
@@ -5,6 +5,6 @@ set -ex
 # https://github.com/SKTBrain/KoBERT/blob/8df69ec6b588ae661bef98d28ec29448482bbe6e/kobert/utils.py#L30
 python export_distilbert.py \
     --model_name "monologg/distilkobert" --tokenizer_type spm \
-    --output_dir ./models --output_model_name distilbert_ko_cased \
+    --output_dir ./models --output_model_name distilkobert_cased \
     --spm_path https://kobert.blob.core.windows.net/models/kobert/tokenizer/kobert_news_wiki_ko_cased-ae5711deb3.spiece \
     --padding_token "[PAD]"
